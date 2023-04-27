c29efef1e9e1ff2740346038e51df18d218c65bb
leetxt13 <leetxt13@naver.com>
2023-04-28 00:54:53 +0900

    play__btn time long

--- main.js
+++ main.js
@@ -251,7 +251,7 @@ play__btn.addEventListener("click", () => {
   randomPosition();
   setTimeout(() => {
     play__btn.style.pointerEvents = "all";
-  }, 11000);
+  }, 12000);
 });
 toIntro__btn.addEventListener("click", () => {
   guidance.classList.remove("turn-out");
